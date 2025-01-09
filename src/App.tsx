d0d87054b10ad9704820d2816545e6852b27a9f9
MuhammadAffan623 <affanmuhammad623@gmail.com>
2025-01-09 18:51:23 +0500

    fix width

--- src/App.tsx
+++ src/App.tsx
@@ -348,7 +348,7 @@ function Home() {
       </div>
 
       <div className="w-full max-w-7xl mx-auto relative">
-        <div className=" py-4 absolute top-[-40px] px-4 ">
+        <div className="w-full py-4 absolute top-[-40px] px-4 mx-auto ">
           {/* First Row of Perks */}
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 sm:gap-6 mb-8">
             {perks.slice(0, 3).map((perk, index) => (
